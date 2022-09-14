a86a532f795a16bdb23bd29902d119cc2d48fbe1
kiwihannah <gkssk8827@gmail.com>
2022-09-14 22:00:21 +0900

    add: test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return status', () => {
+      expect(appController.getHello()).toBe({ status: 'Up & Running 🏃‍♀️' });
     });
   });
 });
